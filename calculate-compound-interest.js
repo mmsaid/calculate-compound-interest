bd4f3940e16eda16c3c60bebbd756005c435d49a
mmsaid <55032499+mmsaid@users.noreply.github.com>
2019-12-20 15:15:32 -0500

    Update and rename compound-interest-calculation.js to calculate-compound-interest.js

--- calculate-compound-interest.js
+++ calculate-compound-interest.js
@@ -2,14 +2,14 @@ function calculate()
 {
     p = document.getElementById("p").value; // princible..
     n = document.getElementById("n").value; // no. of years..
-    q = document.getElementById("q").value; // no. of compoundings per year Or number of periods per year..
+    q = document.getElementById("q").value; // no. of compoundings per year Or no. of periods per year..
     i = document.getElementById("i").value; // interest rate..
     result = document.getElementById("result"); //result..
 
     // The original equation is A = P (1+[i/q]) ^nq
-    // The equation is A = p * [[1 + (i/q)] ^ nq]
+    // The equation is A = p * Math.pow * [[1 + (i/q)] ^ nq]
     A = (p* Math.pow((1 + (i/(q*100))), (n*q)));
-    // toFixed is used for rounding the amount with two decimal places.
+    // toFixed is used for rounding the amount with two decimal.
     result.innerHTML = "The total amount is " + A.toFixed(2);
 
     result.innerHTML += "<br> The interest is " + (A.toFixed(2) - p).toFixed(2);
